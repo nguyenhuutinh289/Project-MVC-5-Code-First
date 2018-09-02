6f7dda2da077433b79b7e34aad5068dbb214351e
nguyenhuutinh289 <nguyenhuutinh289995@gmail.com>
2018-09-02 14:17:22 +0700

    create models

---
+++ TeduShop.Model/Abtract/Auditable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Abtract
+{
+    public abstract class Auditable : IAuditable, ISeoable, ISwitchable
+    {
+        //IAuditable
+        public DateTime? CreateDate { get; set; }
+
+        [MaxLength(256)]
+        public string CreateBy { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        [MaxLength(256)]
+        public string UpdateBy { get; set; }
+
+        // ISeoable
+        [MaxLength(256)]
+        public string MetaKeyWord { get; set; }
+
+        [MaxLength(256)]
+        public string MetaDescription { get; set; }
+
+        //ISwitchable
+        public bool Status { get; set; }
+    }
+}
---
+++ TeduShop.Model/Abtract/IAuditable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeduShop.Model.Abtract
+{
+    public interface IAuditable
+    {
+        DateTime? CreateDate { get; set; }
+        string CreateBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdateBy { get; set; }
+    }
+}
